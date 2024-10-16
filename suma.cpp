68fbef0069455e8730ed270b29e6c8ca3bdd2bdf
Diego <diegoinsano156@gmail.com>
2024-10-16 16:37:19 +0200

    AGREGUE COMENTARIOS Y NOMBRE DEL CREADOR DEL CODIGO

--- suma.cpp
+++ suma.cpp
@@ -1,8 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main () {
+    cout<<"CODIGO CREADO POR DIEGO DE 3RO DE PROGRAMACION";
+    //Se declaran 3 variables
     int a, b, c;
+    //Se leen las variables
     cin>>a>>b>>c;
+    //Imprime las 3 sumas
     cout<< a+b+c;
 
     return 0;
